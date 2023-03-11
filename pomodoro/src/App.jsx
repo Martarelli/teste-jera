35f78d273446d9ab66de6b6b0058a9ed87884561
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2023-03-11 19:51:49 -0300

    exibindo os ciclos concluidos

--- pomodoro/src/App.jsx
+++ pomodoro/src/App.jsx
@@ -3,18 +3,21 @@ import './App.css';
 import sound from './timeover.mp3';
 
 function App() {
-
-  let timePomodoro = 25;
-
+  const [timePomodoro, setTimePomodoro] = useState( 2 );
+  const [cicles, setCicles] = useState( 0 );
   const [totalTimeInSeconds, setTotalTimeInSeconds] = useState( timePomodoro * 60 );
   const [relaxTime, setRelaxTime] = useState(true);
 
   const minutes = Math.floor(totalTimeInSeconds / 60);
   const seconds = totalTimeInSeconds % 60;
   
-  function setTimePomodoro(){
-    
+  const setTime = () => {
+    const value = document.getElementById('time-input').value;
+    setTimePomodoro(value);
+    setTotalTimeInSeconds( value * 60 );
+    setRelaxTime(false);
   }
+
   function playSound(){
     const audio = new Audio(sound);
     audio.currentTime = 0;
@@ -40,6 +43,7 @@ function App() {
           setTotalTimeInSeconds( 5 * 60 );
           setRelaxTime(false);
         } else {
+          setCicles(setCicles + 1);
           setTotalTimeInSeconds( timePomodoro * 60 );
           setRelaxTime(true)
         }
@@ -49,23 +53,24 @@ function App() {
         setTotalTimeInSeconds(totalTimeInSeconds - 1);
       }, 1000);
     }
-  }, [totalTimeInSeconds, relaxTime])
+  }, [totalTimeInSeconds, relaxTime, timePomodoro])
 
   return (
     <div className="App">
+      <div>
+        <span>Ciclos Pomodoro: {cicles}</span>
+      </div>
       <div className='timer'>
         <span>{minutes.toString().padStart(2 , "0")}</span>
         <span>:</span>
         <span>{seconds.toString().padStart(2 , "0")}</span>
       </div>
       <div className='timer-props'>
-        <form action="{}">
           <div className='time-div-inputs'>
-            <label className='time-label'>Tempo Pomodoro: </label>
-            <input className='time-input' type="number" step="1" />
+            <label>Tempo Pomodoro: </label>
+            <input id='time-input' type="number" step="1"/>
           </div>
-          <button className='time-button' type="submit">Alterar</button>
-        </form>
+          <button className='time-button' type="button" onClick={setTime}>Alterar</button>
       </div>
     </div>
   );
